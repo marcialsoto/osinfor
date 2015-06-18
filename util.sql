4d6a50a11cc57ac0d3536f1e0313243fee7ff5fe
root <root@pc-msoto.agrorural.gob.pe>
2015-06-18 10:38:49 -0500

    Commit desde Linux CentOS 6.6

---
+++ util.sql
@@ -0,0 +1,13 @@
+use marcials_osfr;
+/**UPDATE osfr_options SET option_value = 
+replace(option_value, 'http://www2.osinfor.gob.pe/osinfor', 'http://localhost/osinfor') 
+WHERE option_name = 'home' OR option_name = 'siteurl';
+
+UPDATE osfr_posts SET guid = replace(guid, 'http://www2.osinfor.gob.pe/osinfor', 'http://localhost/osinfor');
+
+
+UPDATE osfr_posts SET post_content = 
+replace(post_content, 'http://www2.osinfor.gob.pe/osinfor', 'http://localhost/osinfor');
+**/
+UPDATE osfr_postmeta SET meta_value = 
+replace(meta_value, 'http://www2.osinfor.gob.pe/osinfor', 'http://localhost/osinfor')
